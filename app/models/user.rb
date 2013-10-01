9feca95ff03f8d82961daf2b2ef1836c284a2101
amber-h <amber.houle3@gmail.com>
2013-10-01 16:47:43 -0400

    Making tests for user registration pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :first_name, :last_name, :email, :password
   has_many :projects
+
+  validates_presence_of :password, :on => :create
+  validates_presence_of :first_name, :on => :create
+  validates_presence_of :last_name, :on => :create
+  validates_presence_of :email, :on => :create
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Crowdfunder::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
   resources :projects
+  resources :users, :except => :index
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
