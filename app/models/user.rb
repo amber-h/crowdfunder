491c8e3f6db71ba4f54b36151acf508e01f51c8b
amber-h <amber.houle3@gmail.com>
2013-10-02 12:31:28 -0400

    Generating Pledge model and adding associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :first_name, :last_name, :email, :password
   has_many :projects
-
+  has_many :pledges
+  
   validates_presence_of :password, :on => :create
   validates_presence_of :first_name, :on => :create
   validates_presence_of :last_name, :on => :create
