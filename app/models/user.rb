01795bd206e88415298d0091b89af56431cb51f7
amber-h <amber.houle3@gmail.com>
2013-10-03 14:49:21 -0400

    Making tests for user mailer pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name, :on => :create
   validates_presence_of :last_name, :on => :create
   validates_presence_of :email, :on => :create
+
+  def name
+  	first_name + last_name
+  end
 end
